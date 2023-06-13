1a74635210f5f7c51c14b79c089f22955ad86338
Maciej Boron <maciej.boron.22@gmail.com>
2023-06-13 19:32:54 +0200

    ability to raise by any amount

--- script.js
+++ script.js
@@ -4,20 +4,33 @@ function add_player() {
     const name = document.createElement("td");
     name.innerHTML = "Player " + number_of_players
     document.getElementById("players").appendChild(name);
+
     const money = document.createElement("td");
-    money.innerHTML = "20000";
+    money.innerHTML = 20000;
+    money.id = "money" + number_of_players;
     document.getElementById("money").appendChild(money);
+
     const bet = document.createElement("td");
-    bet.innerHTML = "0";
+    bet.innerHTML = 0;
+    bet.id = "bet" + number_of_players;
     document.getElementById("bets").appendChild(bet);
+
     const raise = document.createElement("td");
     const input_box = document.createElement("input");
     input_box.type = "number";
+    input_box.id = "raise" + number_of_players;
     const raise_button = document.createElement("button");
     raise_button.innerHTML = "raise";
+    raise_button.onclick = function() {
+        var raise_val = document.getElementById("raise" + number_of_players).value;
+        document.getElementById("money" + number_of_players).innerHTML -= raise_val;
+        document.getElementById("bet" + number_of_players).innerHTML -= -raise_val;
+        document.getElementById("raise" + number_of_players).value = 0;
+    }
     raise.appendChild(input_box);
     raise.appendChild(raise_button);
     document.getElementById("raise").appendChild(raise);
+
     const win = document.createElement("td");
     const win_button = document.createElement("button");
     win_button.innerHTML = "win";
